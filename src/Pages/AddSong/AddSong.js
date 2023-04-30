0bd420866c5ed723c320afc8b1b9484348303708
Elijah Larios <elijahlarios@icloud.com>
2023-04-30 16:29:44 -0700

    change to SONGID instead of songLink

--- src/Pages/AddSong/AddSong.js
+++ src/Pages/AddSong/AddSong.js
@@ -13,16 +13,10 @@ function AddSong() {
     const handleSubmit = async (e) => {
       e.preventDefault();
       try {
-        // Send the song link to the API and store the result in local storage
-        const response = await fetch(API.TUNITAPI, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ songLink })
-        });
+        // Send the song ID to the API and store the result in local storage
+        const response = await fetch(`${API.TUNITAPI}/${songLink}`);    
         const data = await response.json();
-        localStorage.setItem('songData', JSON.stringify(data));
+        localStorage.setItem('recData', JSON.stringify(data));
         alert('Song data stored in local storage.');
       } catch (error) {
         console.error(error);
@@ -48,9 +42,13 @@ function AddSong() {
             <Navbar />
             <div className='addSong-container'>
                 <br></br>
-                <h1>Paste Song Below</h1>
+                <h1>Add Song</h1>
+                <h2 className='addSong-h2'>
+                    Type and select the name of your favorite song below and
+                    we'll find the perfect recommendations for you!
+                </h2>
                 <br></br>
-                <input className='input-addSong' placeholder='music.apple.com/songLink' />
+                <input className='input-addSong' placeholder='Flatbed Freestyle' />
                 <button className='button-submit' type='submit' onClick={handleSubmit}>
                     Submit
                 </button>

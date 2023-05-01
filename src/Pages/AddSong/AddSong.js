6ea5fb7a12bac33adb7b745f9de9006141059121
Elijah Larios <elijahlarios@icloud.com>
2023-04-30 19:39:35 -0700

    temp push until API update

--- src/Pages/AddSong/AddSong.js
+++ src/Pages/AddSong/AddSong.js
@@ -4,30 +4,25 @@ import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../../Firebase/Config";
+import { Axios } from 'axios';
 import * as API from './Api/apiReference';
 
 function AddSong() {
 
-    const [songLink, setSongLink] = useState('');
-
+    const [songID, setSongLink] = useState('');
+    const tempID = '7lmeHLHBe4nmXzuXc0HDjk';
     const handleSubmit = async (e) => {
-      e.preventDefault();
-      try {
-        // Send the song ID to the API and store the result in local storage
-        const response = await fetch(`${API.TUNITAPI}/${songLink}`, {mode: 'cors'});    
-        const data = await response.json();
-        localStorage.setItem('recData', JSON.stringify(data));
-        alert('Song data stored in local storage.');
-      } catch (error) {
-        console.error(error);
-        alert('Error storing song data.');
-      }
-    };
-  
-    const handleInputChange = (e) => {
-        setSongLink(e.target.value);
-    };
-        
+        e.preventDefault();
+        try {
+          const input = document.querySelector('.input-addSong').value;
+          const response = await fetch(`${API.TUNITAPI}/${input}`);
+          const data = await response.json();
+          console.log(data);
+          // Use the data to set the song ID state or do any other operations
+        } catch (error) {
+          console.error(error);
+        }
+      };
         
     const [user, loading, error] = useAuthState(auth);
     const [name, setName] = useState("");
